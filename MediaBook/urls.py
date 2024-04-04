a95e9be5d9f9f245cfa2f23457d02761f67821ed
Kinjen0 <118626914+Kinjen0@users.noreply.github.com>
2024-04-04 11:43:15 -0400

    Add login/logout/register features

--- MediaBook/urls.py
+++ MediaBook/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+    path('' , views.home, name='home'),
+    
+    ]
+
+
+
