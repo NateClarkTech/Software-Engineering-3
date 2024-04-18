18c22a2f39f3829cd80f8819c96f335954708436
BilgeBengisu <76906763+BilgeBengisu@users.noreply.github.com>
2024-04-17 20:12:16 -0400

    implemented label selection, sort and toggle on sound application

--- Sound/views.py
+++ Sound/views.py
@@ -7,17 +7,22 @@ from Sound.models import *
 
 from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+# Bilge implemented this application.
 
-#Bilge
 @login_required
 def sound(request):
     if request.method == "POST":
         if "soundNoteSubmit" in request.POST:
             title = request.POST.get('title', '').strip() # get the note title
             description = request.POST.get('description', '').strip()
-            if title or description:  
-                SoundNote.objects.create(note_title=title, note_description=description)
+            labelselect = request.POST.get("labelselect")
+            label = get_object_or_404(SoundLabel, label_name=labelselect)
+            if title or description or label:  
+                SoundNote.objects.create(note_title=title, note_description=description, note_label=label)
+        if "soundCreateLabel" in request.POST:
+            label = request.POST.get('createLabel', "").strip()
+            if label:
+                SoundLabel.objects.create(label_name=label)
         return redirect('sound')
     else:
         notes = noteQuery()
@@ -29,3 +34,8 @@ def noteQuery():
     return SoundNote.objects.all().order_by('-created_at')
 def labelQuery():
     return SoundLabel.objects.all().order_by("label_name")
+
+@login_required
+def label_sort(request, label_name):
+    label = SoundLabel.objects.get(label_name=label_name)
+    return render(request, 'label_sort.html', {'label': label})
